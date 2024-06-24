60129fbd9a005c1c042e6e9d9ba0b3090f43a8ea
guslarra01 <gustavolarragueta@gmail.com>
2024-06-24 11:21:31 -0300

    Update ejercicio3.py
    Signed-off-by: guslarra01 <gustavolarragueta@gmail.com>

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,19 @@
+class Articulo:
+    def __init__(self, nombre, puntos):
+        self.nombre = nombre
+        self.puntos = puntos
+        self.siguiente = None 
 
+    def __repr__(self):
+        return f"Articulo({self.nombre}, {self.puntos})"
+      ss Comprador:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.articulos_comprados = []
+
+    def comprar_articulo(self, articulo, cantidad):
+        self.articulos_comprados.extend([articulo] * cantidad)
+    def puntaje_total(self):
+        return sum(articulo.puntos for articulo in self.articulos_comprados)
+      def __repr__(self):
+        return f"Comprador({self.nombre})"
