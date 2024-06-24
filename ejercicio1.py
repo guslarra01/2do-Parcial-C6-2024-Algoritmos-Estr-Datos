2f62be556c92938641d24097bfd2034975908fc5
guslarra01 <gustavolarragueta@gmail.com>
2024-06-24 10:15:45 -0300

    gustavo ejercicio1.py
    Signed-off-by: guslarra01 <gustavolarragueta@gmail.com>

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,7 @@
-
+class Comprador:
+    def __init__(self, nombre, direccion, telefono, fecha_nacimiento):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.fecha_nacimiento = fecha_nacimiento
+        self.puntaje = 0
